d8e50f9198debd1e90f788cd8b41ec0f331867f9
Keichi Takahashi <keichi.t@me.com>
2017-04-07 12:19:49 +0900

    Add wrapper functions for Fortran

--- src/oxton.cpp
+++ src/oxton.cpp
@@ -227,3 +227,78 @@ int register_comm(MPI_Comm comm)
 
     return EXIT_SUCCESS;
 }
+
+extern "C" void mpi_finalize_(MPI_Fint *ierr)
+{
+    int c_ierr = MPI_Finalize();
+    if (NULL != ierr) *ierr = c_ierr;
+}
+
+extern "C" void mpi_init_(MPI_Fint *ierr)
+{
+    int c_ierr;
+    int argc = 0;
+    char **argv = NULL;
+
+    c_ierr = MPI_Init(&argc, &argv);
+    if (NULL != ierr) *ierr = c_ierr;
+}
+
+extern "C" void mpi_comm_create_f(MPI_Fint *comm, MPI_Fint *group,
+                                  MPI_Fint *newcomm, MPI_Fint *ierr)
+{
+    int c_ierr;
+    MPI_Comm c_newcomm;
+    MPI_Comm c_comm = PMPI_Comm_f2c (*comm);
+    MPI_Group c_group = PMPI_Group_f2c(*group);
+
+    c_ierr = MPI_Comm_create(c_comm, c_group, &c_newcomm);
+    if (NULL != ierr) *ierr = c_ierr;
+
+    if (MPI_SUCCESS == c_ierr) {
+        *newcomm = PMPI_Comm_c2f(c_newcomm);
+    }
+}
+
+extern "C" void mpi_comm_dup_(MPI_Fint *comm, MPI_Fint *newcomm, MPI_Fint *ierr)
+{
+    int c_ierr;
+    MPI_Comm c_newcomm;
+    MPI_Comm c_comm = PMPI_Comm_f2c(*comm);
+
+    c_ierr = MPI_Comm_dup(c_comm, &c_newcomm);
+    if (NULL != ierr) *ierr = c_ierr;
+
+    if (MPI_SUCCESS == c_ierr) {
+        *newcomm = PMPI_Comm_c2f(c_newcomm);
+    }
+}
+
+extern "C" void mpi_comm_split_(MPI_Fint *comm, MPI_Fint *color,
+                                 MPI_Fint *key, MPI_Fint *newcomm,
+                                 MPI_Fint *ierr)
+{
+    int c_ierr;
+    MPI_Comm c_newcomm;
+    MPI_Comm c_comm = PMPI_Comm_f2c (*comm);
+
+    c_ierr = MPI_Comm_split(c_comm, *color, *key, &c_newcomm);
+    if (NULL != ierr) *ierr = c_ierr;
+
+    if (MPI_SUCCESS == c_ierr) {
+        *newcomm = PMPI_Comm_c2f(c_newcomm);
+    }
+ }
+
+extern "C" void mpi_comm_free_(MPI_Fint *comm, MPI_Fint *ierr)
+{
+    int c_ierr;
+    MPI_Comm c_comm = PMPI_Comm_f2c(*comm);
+
+    c_ierr = MPI_Comm_free(&c_comm);
+    if (NULL != ierr) *ierr = c_ierr;
+
+    if (MPI_SUCCESS == c_ierr) {
+        *comm = PMPI_Comm_c2f(c_comm);
+    }
+}
